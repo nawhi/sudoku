bc444090c9da273301508cb8e571635c76c404f2
nawhi <25394965+nawhi@users.noreply.github.com>
2024-08-10 17:17:24 +0100

    Add checker to complete infrastructure setup

--- core/checker.py
+++ core/checker.py
@@ -1,5 +1,22 @@
-from core.model.board import SudokuBoard
+from core.model.board import SudokuBoard, subgrid_of_board
 
 
 def is_solved(board: SudokuBoard) -> bool:
-    raise NotImplementedError("Implement me!")
+    if 0 in board:
+        return False
+
+    for row in board:
+        if len(set(row)) != 9:
+            return False
+
+    for column in board.transpose():
+        if len(set(column)) != 9:
+            return False
+
+    for i in range(3):
+        for j in range(3):
+            subgrid = subgrid_of_board(board, i, j)
+            if len(set(subgrid.flatten())) != 9:
+                return False
+
+    return True
--- test/test_checker.py
+++ test/test_checker.py
@@ -1,6 +1,87 @@
-from test.util import SudokuTestCase
+from core.checker import is_solved
+from core.parser import parse_board
+from test.util import SudokuTestCase, board_of_first_row
 
 
 class TestChecker(SudokuTestCase):
-    def test_todo(self):
-        raise NotImplementedError
+    def test_is_not_solved_if_board_has_blanks(self):
+        self.assertFalse(is_solved(board_of_first_row('.........')))
+
+    def test_is_solved_if_board_has_all_numbers(self):
+        board = parse_board("""
+            ┌───────┬───────┬───────┐
+            │ 1 2 3 │ 4 5 6 │ 7 8 9 │
+            │ 4 5 6 │ 7 8 9 │ 1 2 3 │
+            │ 7 8 9 │ 1 2 3 │ 4 5 6 │
+            ├───────┼───────┼───────┤
+            │ 2 3 1 │ 5 6 4 │ 8 9 7 │
+            │ 5 6 4 │ 8 9 7 │ 2 3 1 │
+            │ 8 9 7 │ 2 3 1 │ 5 6 4 │
+            ├───────┼───────┼───────┤
+            │ 3 1 2 │ 6 4 5 │ 9 7 8 │
+            │ 6 4 5 │ 9 7 8 │ 3 1 2 │
+            │ 9 7 8 │ 3 1 2 │ 6 4 5 │
+            └───────┴───────┴───────┘
+        """)
+        self.assertTrue(is_solved(board))
+
+    def test_is_not_solved_if_duplicate_number_in_row(self):
+        # note two nines in row 9
+        board = parse_board("""
+            ┌───────┬───────┬───────┐
+            │ 1 2 3 │ 4 5 6 │ 7 8 9 │
+            │ 4 5 6 │ 7 8 9 │ 1 2 3 │
+            │ 7 8 9 │ 1 2 3 │ 4 5 6 │
+            ├───────┼───────┼───────┤
+            │ 2 3 1 │ 5 6 4 │ 8 9 7 │
+            │ 5 6 4 │ 8 9 7 │ 2 3 1 │
+            │ 8 9 7 │ 2 3 1 │ 5 6 4 │
+            ├───────┼───────┼───────┤
+            │ 3 1 2 │ 6 4 5 │ 9 7 8 │
+            │ 6 4 5 │ 9 7 8 │ 3 1 2 │
+            │ 9 7 8 │ 3 1 2 │ 6 4 9 │
+            └───────┴───────┴───────┘
+        """)
+
+        self.assertFalse(is_solved(board))
+
+    def test_is_not_solved_if_duplicate_number_in_column(self):
+        # note two nines in column 1
+        board = parse_board("""
+            ┌───────┬───────┬───────┐
+            │ 9 2 3 │ 4 5 6 │ 7 8 9 │
+            │ 4 5 6 │ 7 8 9 │ 1 2 3 │
+            │ 7 8 9 │ 1 2 3 │ 4 5 6 │
+            ├───────┼───────┼───────┤
+            │ 2 3 1 │ 5 6 4 │ 8 9 7 │
+            │ 5 6 4 │ 8 9 7 │ 2 3 1 │
+            │ 8 9 7 │ 2 3 1 │ 5 6 4 │
+            ├───────┼───────┼───────┤
+            │ 3 1 2 │ 6 4 5 │ 9 7 8 │
+            │ 6 4 5 │ 9 7 8 │ 3 1 2 │
+            │ 9 7 8 │ 3 1 2 │ 6 4 5 │
+            └───────┴───────┴───────┘
+        """)
+
+        self.assertFalse(is_solved(board))
+
+    def test_is_not_solved_if_duplicate_numbers_in_subgrid(self):
+        # duplicate 3 in middle top and
+        # duplicate 4 in middle bottom
+        board = parse_board("""
+            ┌───────┬───────┬───────┐
+            │ 1 2 4 │ 3 5 6 │ 7 8 9 │
+            │ 4 5 6 │ 7 8 9 │ 1 2 3 │
+            │ 7 8 9 │ 1 2 3 │ 4 5 6 │
+            ├───────┼───────┼───────┤
+            │ 2 3 1 │ 5 6 4 │ 8 9 7 │
+            │ 5 6 3 │ 8 9 7 │ 2 4 1 │
+            │ 8 9 7 │ 2 3 1 │ 5 6 4 │
+            ├───────┼───────┼───────┤
+            │ 3 1 2 │ 6 4 5 │ 9 7 8 │
+            │ 6 4 5 │ 9 7 8 │ 3 1 2 │
+            │ 9 7 8 │ 4 1 2 │ 6 3 5 │
+            └───────┴───────┴───────┘
+        """)
+
+        self.assertFalse(is_solved(board))
